910f5094be68c36c704c5ba79ab94fa9173a5e2e
test <labanisardar@gmail.com>
2024-10-01 13:50:40 +0530

    allow cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use('/static', express.static(path.join(__dirname, 'images')));
 
 app.use(cors({
-    origin: 'https://happy-bags-4.onrender.com', // Replace with your actual frontend URL
+    origin: ['https://eloquent-cocada-495f93.netlify.app', 'http://localhost:3000'], // Replace with your actual frontend URL
     credentials: true,
     methods: ['POST', 'PATCH', 'GET', 'OPTIONS', 'DELETE']
 }));
