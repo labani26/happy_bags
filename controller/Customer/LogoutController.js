b01a41c89e519aaec868dab25fd2c499bb232160
test <labanisardar@gmail.com>
2024-10-02 13:15:04 +0530

    new logout

--- controller/Customer/LogoutController.js
+++ controller/Customer/LogoutController.js
@@ -2,23 +2,15 @@
 
 const logout = (req, res) => {
 
-    if(req.session._id) {
-        req.session.destroy((error) => {
-
-            if (error) {
-                return res.status(500).json({ message: "Failed to log out." });
-            }
-    
-            // Optionally, clear the cookie for better session management
-            // res.clearCookie('connect.sid', { path: '/' });
-    
-            res.status(200).json({ message: "Logout successful" });
-        });
-
+    if (error) {
+        return res.status(500).json({ message: "Failed to log out." });
     }
-    else {
-        return res.status(500).json({ error: "No one logged in" });
-    }
-};
 
-module.exports = {logout};
+    // Optionally, clear the cookie for better session management
+    // res.clearCookie('connect.sid', { path: '/' });
+
+    res.status(200).json({ message: "Logout successful" })
+
+ };
+
+module.exports = { logout };
