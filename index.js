8a2a447c6c8020e63219d8df06bd4a69a9e1dca2
test <labanisardar@gmail.com>
2024-10-09 13:10:15 +0530

    updted index

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello Bags");
 });
 
 MongoConnect(); // Ensure MongoConnect is correctly setting up your database connection
